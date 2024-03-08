7241bdedce49b38672a92b022b602ac6b7a2955d
Enea Xharja <19818937+eneax@users.noreply.github.com>
2024-03-08 14:15:46 +0100

    add inline_markdown module and corresponding unit tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -4,6 +4,7 @@ from textnode import (
     TextNode,
     text_type_text,
     text_type_bold,
+    text_type_italic
 )
 
 
@@ -24,8 +25,8 @@ class TestTextNode(unittest.TestCase):
         self.assertNotEqual(node, node2)
 
     def test_eq_url(self):
-        node = TextNode("This is a text node", text_type_text, "https://www.boot.dev")
-        node2 = TextNode("This is a text node", text_type_text, "https://www.boot.dev")
+        node = TextNode("This is a text node", text_type_italic, "https://www.boot.dev")
+        node2 = TextNode("This is a text node", text_type_italic, "https://www.boot.dev")
         self.assertEqual(node, node2)
 
     def test_repr(self):
