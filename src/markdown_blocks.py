958705ad9d0d46447c7bc9fbfd019fd2d7294f98
Enea Xharja <19818937+eneax@users.noreply.github.com>
2024-03-09 15:54:27 +0100

    refactor text_node_to_html_node function

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,6 +1,6 @@
 from htmlnode import ParentNode
 from inline_markdown import text_to_textnodes
-from textnode import TextNode
+from textnode import text_node_to_html_node
 
 block_type_paragraph = "paragraph"
 block_type_heading = "heading"
@@ -90,7 +90,7 @@ def text_to_children(text):
     text_nodes = text_to_textnodes(text)
     children = []
     for text_node in text_nodes:
-        html_node = TextNode.text_node_to_html_node(text_node)
+        html_node = text_node_to_html_node(text_node)
         children.append(html_node)
     return children
 
